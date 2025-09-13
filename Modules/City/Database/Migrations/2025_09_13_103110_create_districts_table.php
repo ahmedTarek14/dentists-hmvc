fcaedb28c8308784c77aa95756f0cf851ac46909
ahmedTarek14 <ahmedtarek.am@gmail.com>
2025-09-13 16:12:40 +0300

    same

--- Modules/City/Database/Migrations/2025_09_13_103110_create_districts_table.php
+++ Modules/City/Database/Migrations/2025_09_13_103110_create_districts_table.php
@@ -14,6 +14,7 @@ class CreateDistrictsTable extends Migration
     public function up()
     {
         Schema::create('districts', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->id();
             $table->foreignId('city_id')->constrained('cities')->onDelete('cascade');
             $table->string('name');
